b26805e5e425f907cdc37447ed85b265785f9032
David Sin <davidsin1990@gmail.com>
2014-05-08 15:30:43 -0400

    added up to 2

--- week_3/1_code_combat/my_solution.js
+++ week_3/1_code_combat/my_solution.js
@@ -3,9 +3,24 @@
 // For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
- 
- 
- 
+
+1. Rescue Mission
+move down 2x
+move right
+move up 2x
+move right 2x
+move down 2x
+Attack!
+
+2. Grab the Mushroom
+
+move up 1x
+move right 1x
+move left 1x
+move up 1x
+Attack!
+
+3. Drink Me
  
  
  
