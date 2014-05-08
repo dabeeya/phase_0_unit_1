a4b6976a96ec731ae4908252a62330e03606c161
David Sin <davidsin1990@gmail.com>
2014-05-08 19:38:35 -0400

    added more combat code

--- week_3/1_code_combat/my_solution.js
+++ week_3/1_code_combat/my_solution.js
@@ -5,27 +5,113 @@
 // command + / This will comment the line. 
 
 1. Rescue Mission
-move down 2x
-move right
-move up 2x
-move right 2x
-move down 2x
-Attack!
+// //move down 2x
+// //move right
+// //move up 2x
+// //move right 2x
+// //move down 2x
+// Attack!
 
-2. Grab the Mushroom
+// 2. Grab the Mushroom
 
-move up 1x
-move right 1x
-move left 1x
-move up 1x
-Attack!
+// move up 1x
+// move right 1x
+// move left 1x
+// move up 1x
+// Attack!
 
-3. Drink Me
+// 3. Drink Me
+// move right 1x
+// Attack!
+// move right 1x
+// move down 1x
+// move up 1x
+// move right 1x
+// Attack!
+
+// 4. Taunt the Guards
+// move right 1x
+// bust down door
+// move right 1x
+// say Hey There
+// move left 2x
+// Attack!
+// move right1x
+// say Follow Me
+// move right 2x
+// move up 2x
+// move right 1x
+// say Oi
+// move down 1x
+// move right 1x
+// move up 1x
+// move right 2x
+ 
+// 5. Its a Trap
+// move down 2x
+// say Taunt
+// move up 2x
+
+// 6. Taunt
+// say Taunt 4x
+
+// 7. Cowardly Taunt
+// moveXY(50, 16)
+// moveXY(63, 20)
+// moveXY(70, 10)
+// say Lure
+// moveXY(55,32)
+// say Follow
+// moveXY(70,10)
+
+// 8. Commanding Followers
+// moveXY(49, 66);
+// moveXY(60, 63);
+// moveXY(75, 63);
+// say Hail, friends
+// say Follow
+// moveXY(76,42)
+// say Follow
+// say Attack!
+
+// 9. Mobile Artillery
+// moveXY(30, 26)
+// moveXY(68, 34)
+// attackXY(69,56)
+// moveXY(42, 41)
+// attackXY(50, 67)
+// attackXY(46, 54)
  
  
  
- 
- 
- 
+1. In code combat, "this" is referring to the knight. In Javascript, this 
+usually refers to an object. The object in code combats case is a variable.
+2. The () are used to hold the parameters in the function. 
+3. The point of semicolons is let Javascript know that we are ending the statement. In Ruby, using ; was not 
+as mandatory. However, it seems that in Javascript, we will need to use the semicolon much more often, if 
+not all the time.
+
 // Reflection:
-// Write your reflection here.
+// Write your reflection here
+
+What parts of your strategy worked? What problems did you face?
+- There weren't any real issues or problems I had to face when engaging in code combat because it was a
+simple intro to Javascript. I just had to follow the instructions and use logical reasoning. 
+What questions did you have while coding? What resources did you find to help you answer them?
+- A question I had while coding was what the parenthesis were there for. I found it quite puzzling that
+they were just blank for all of the code we wrote. However, when I started the next section I realized 
+what they were for.
+What concepts are you having trouble with, or did you just figure something out? If so, what?
+- From this section, there wasn't anything that I had trouble with. 
+Did you learn any new skills or tricks?
+- Not really. I learned some extreme basics of Javascript.
+How confident are you with each of the Learning Competencies?
+- So far I feel pretty confident with the first 2 sections of week 3. However, I also did get a lot of help
+during office hours. I still need to practice and reread the guides and articles given from DBC.
+Which parts of the challenge did you enjoy?
+- I found code combat pretty interesting. I think it was pretty fun because it was very interactive and had
+me seeing what each piece of code did. Even though it was a pretty basic tutorial, I enjoyed it because it 
+have me feeling lost. It was a great way to introduce Javascript, at least in my opinion. 
+Which parts of the challenge did you find tedious?
+- I found writing each syntax for each mission very tedious especially because it became quite repetitious. Even
+though we were learning something new each mission, we still had to write each syntax over and over again.
