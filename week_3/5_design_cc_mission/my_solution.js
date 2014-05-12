d528444435dd13c30c3aa20ebafcba4990552311
David Sin <davidsin1990@gmail.com>
2014-05-11 22:38:47 -0400

    mission

--- week_3/5_design_cc_mission/my_solution.js
+++ week_3/5_design_cc_mission/my_solution.js
@@ -1,37 +1,97 @@
 // I worked [by myself, with:] on this challenge
 
 // Your mission description:
-// 
-// 
-// 
+// The mission is to find the princess in a specific, and then fight an orge to get away. However, Tharin
+// cannot defeat the orge with brute force. The ogre asks a riddle and will let him pass if he answers 
+// correctly.
 
 // Pseudocode
-// 
-// 
-// 
-// 
-// 
+// Tharin walks up 2x
+// Tharin walks right 2x
+// Tharin calls the princess
+// Tharin attacks the ogre that entered room
+// Tharin answers riddle
 
 // Initial Code
+var this = {Tharin};
+var princess = "Princess";
+var ogre = "Ogre";
+var riddle = true;
+
+this.moveUp();
+this.moveUp();
+this.moveRight();
+this.moveRight();
 
 
+this.say("Is this the Princess");
+if (name === princess)
+	return true;
+this.say("Follow me!")
 
+this.attackNearbyEnemy;
 
+this.say("I cannot defeat you");
+ogre.say("I will let you pass if you answer this riddle")
 
+while (riddle)
+	var hint = prompt("What can you catch but can not throw?");
+    if (hint === "a cold") {
+        console.log("Darn it! You can leave with your princess");
+
+this.moveUp();
+this.moveUp();
+this.moveLeft();
 
 // Refactored Code
+var this = {Tharin};
+var princess = "Princess";
+var ogre = "Ogre";
+var riddle = true;
 
+this.moveUp();
+this.moveUp();
+this.moveRight();
+this.moveRight();
+
+
+this.say("Is this the Princess");
+if (name === princess)
+	return true;
+this.say("Follow me!")
+
+this.attackNearbyEnemy;
+
+this.say("I cannot defeat you");
+ogre.say("I will let you pass if you answer this riddle")
+
+while (riddle)
+	var hint = prompt("What can you catch but can not throw?");
+    if (hint === "a cold") {
+        console.log("Darn it! You can leave with your princess");
+
+this.moveUp();
+this.moveUp();
+this.moveLeft();
 
 
 
 
 
 // Reflection
-// 
-// 
-// 
-// 
-// 
-// 
+// - So I wanted to make a pretty simple mission with the material I learned, so I went back to code combact to try and
+// create a mission similar to the ones we had to complete. But I also wanted to add a twist with Tharin having to
+// answer a riddle. Some problems I faced was that I was not sure if I was able to check this code to see if 
+// if there were any issues with the code. Also, I felt that because I used really simple code, there was
+// nothing to really refactor.
+// - I think I'm having some trouble understanding the flow of the code I created. I feel like if we were to run
+// this, the system would crash because I left something out or that the code just didnt make snese.
+// - I was able to use the prompt screen which we learned about when reading the intro guide to Javascript which
+// I thought was a pretty cool function. 
+// - I am not too confident about this challenge. I'm not sure if the syntax I wrote would work. I was not sure
+// if there was a way to test to make sure there wasn't a mistake.
+// - I found this challegene to be pretty exciting. It allowed me to have to be creative and incorporate my creativenss
+// with the code I had to create. 
+// - I found the psuedcoding to be pretty tedious because I almost felt that it was kind of uncessary. 
 // 
 // 
